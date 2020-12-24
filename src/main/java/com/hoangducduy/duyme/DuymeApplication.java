d6a037bd935af18450248f36f0d165201965b405
Duy <duyme18@gmail.com>
2020-12-24 15:38:13 +0700

    first commit

---
+++ src/main/java/com/hoangducduy/duyme/DuymeApplication.java
@@ -0,0 +1,13 @@
+package com.hoangducduy.duyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoangducduy/duyme/DuymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangducduy.duyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
